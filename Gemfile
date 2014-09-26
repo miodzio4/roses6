10a0d5085ccb669cdfe33c57394dd787869a37ac
Your Name <your.email@example.com>
2014-09-26 23:59:33 +0200

    first commit

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source 'https://rubygems.org'
+gem 'rails', '3.2.1'
+gem 'pg', '0.17.1'
+gem 'gravatar_image_tag', '1.0.0.pre2'
+gem 'formtastic', '2.2'
+gem 'nifty-generators', '0.4.6'
+
+group :assets do
+	gem 'sass-rails', '~> 3.2.3'
+	gem 'coffee-rails', '~> 3.2.1'
+	gem 'therubyracer'
+	gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails', '3.1.2'
+gem "mocha", :group => :test
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Roses6::Application.config.secret_token = 'd0968da401eb657e4376534da4f6df1fb6ccd5f4ba3d0a0d6378de276a4a186a505aae82cbc846c29b8d2e5d6da4412df5898f6763c615d8df01b23b73d50a2f'
